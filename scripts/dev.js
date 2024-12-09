b46d56b67e626b8696117c1c5beffcd4f7a0436e
林嘉伦 <linjialun@kingsware.cn>
2024-12-09 18:02:22 +0800

    feat: 兼容node一些语法

--- scripts/dev.js
+++ scripts/dev.js
@@ -9,5 +9,10 @@ console.log(target, format);
 // node中esm模块没有 __dirname、__filename、require
 // 兼容node的方法
 import { fileURLToPath } from "url";
-// 获取文件的绝对路径 file: -> /user
+import { resolve, dirname } from "path";
+import { createRequire } from "module";
+// 获取文件的绝对路径（提供的是当前文件的完整路径，包括文件名）
 const __filename = fileURLToPath(import.meta.url);
+// 获取文件的目录路径（提供的是当前文件所在目录的路径，但不包括文件名）
+const __dirname = dirname(__filename)
+const require = createRequire(import.meta.url)
