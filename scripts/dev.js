fd7f086ae3a84525dc62dd037b51665fc3e997e4
林嘉伦 <linjialun@kingsware.cn>
2024-12-11 10:55:02 +0800

    build: 配置package.json

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,11 +1,3 @@
-import minimist from "minimist";
-const args = minimist(process.argv.slice(2));
-
-const target = args._[0] || "reactivity"; // 打包哪个项目
-const format = args.f || "iife"; // 打包后的模块化规范
-console.log(target, format);
-
-
 // node中esm模块没有 __dirname、__filename、require
 // 兼容node的方法
 import { fileURLToPath } from "url";
@@ -16,3 +8,8 @@ const __filename = fileURLToPath(import.meta.url);
 // 获取文件的目录路径（提供的是当前文件所在目录的路径，但不包括文件名）
 const __dirname = dirname(__filename)
 const require = createRequire(import.meta.url)
+
+import minimist from "minimist";
+const args = minimist(process.argv.slice(2));
+const target = args._[0] || "reactivity"; // 打包哪个项目
+const format = args.f || "iife"; // 打包后的模块化规范
